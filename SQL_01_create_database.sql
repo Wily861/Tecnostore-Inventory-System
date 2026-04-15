235ab55f135fc255c7d300aa59815de6ef5e378a
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 17:22:46 -0500

    Update SQL_01_create_database.sql

--- SQL_01_create_database.sql
+++ SQL_01_create_database.sql
@@ -1,1 +1,12 @@
+-- ============================================================
+-- 🛒 TecnoStore S.A.S – Script de Creación de Base de Datos
+-- Autor: Wily Duvan Villamil Rey
+-- Fecha: Agosto 2025
+-- Descripción: Definición del esquema relacional del sistema
+-- ============================================================
 
+-- ================================
+-- CREACIÓN DE BASE DE DATOS
+-- ================================
+CREATE DATABASE IF NOT EXISTS tecnostore;
+USE tecnostore;
