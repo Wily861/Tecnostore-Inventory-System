20234364a53a02da891b1318e59af17cca6548d8
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 17:33:08 -0500

    Update 05_create_customers_table.sql

--- cripts/05_create_customers_table.sql
+++ cripts/05_create_customers_table.sql
@@ -1,1 +1,9 @@
-
+-- ================================
+-- TABLA: Cliente
+-- ================================
+CREATE TABLE Cliente (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nombre     VARCHAR(100) NOT NULL,
+    correo     VARCHAR(100) UNIQUE,
+    telefono   VARCHAR(20)
+);
