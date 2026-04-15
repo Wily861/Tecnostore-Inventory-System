18d7a31528da7cafcba84d99ab51945f1bc6e247
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 17:31:34 -0500

    Update 03_create_product_entries_table.sql

--- cripts/03_create_product_entries_table.sql
+++ cripts/03_create_product_entries_table.sql
@@ -1,1 +1,11 @@
-
+-- ================================
+-- TABLA: Entrada_Producto
+-- ================================
+CREATE TABLE Entrada_Producto (
+    id_entrada     INT AUTO_INCREMENT PRIMARY KEY,
+    fecha          DATE NOT NULL,
+    cantidad       INT NOT NULL,
+    detalle_entrada VARCHAR(255),
+    id_proveedor   INT,
+    FOREIGN KEY (id_proveedor) REFERENCES Proveedor(id_proveedor)
+);
