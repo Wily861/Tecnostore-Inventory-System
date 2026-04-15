0dda7a39639ffa227b3b8b39041532c1e05415ab
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 17:30:53 -0500

    Update 02_create_vendors_table.sql

--- cripts/02_create_vendors_table.sql
+++ cripts/02_create_vendors_table.sql
@@ -1,1 +1,9 @@
-
+-- ================================
+-- TABLA: Proveedor
+-- ================================
+CREATE TABLE Proveedor (
+    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
+    nombre       VARCHAR(100) NOT NULL,
+    contacto     VARCHAR(100),
+    direccion    VARCHAR(150)
+);
