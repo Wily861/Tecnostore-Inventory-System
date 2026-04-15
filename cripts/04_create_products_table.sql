193df87d25f284740b10d15378ff9c3a8eea265a
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 17:32:22 -0500

    Update 04_create_products_table.sql

--- cripts/04_create_products_table.sql
+++ cripts/04_create_products_table.sql
@@ -1,1 +1,12 @@
-
+-- ================================
+-- TABLA: Producto
+-- ================================
+CREATE TABLE Producto (
+    id_producto    INT AUTO_INCREMENT PRIMARY KEY,
+    codigo_interno VARCHAR(50) UNIQUE NOT NULL,
+    descripcion    VARCHAR(200),
+    marca          VARCHAR(100),
+    modelo         VARCHAR(100),
+    precio         DECIMAL(10,2) NOT NULL,
+    stock          INT DEFAULT 0
+);
