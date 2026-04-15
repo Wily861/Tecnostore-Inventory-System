f0e8c98e633711eb34f348d7cfc5abde407712d8
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 17:33:47 -0500

    Update 06_create_budgets_table.sql

--- cripts/06_create_budgets_table.sql
+++ cripts/06_create_budgets_table.sql
@@ -1,1 +1,11 @@
-
+-- ================================
+-- TABLA: Presupuesto
+-- ================================
+CREATE TABLE Presupuesto (
+    id_presupuesto INT AUTO_INCREMENT PRIMARY KEY,
+    fecha          DATE NOT NULL,
+    validez        DATE,
+    total_estimado DECIMAL(12,2),
+    id_cliente     INT,
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+);
