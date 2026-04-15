4c26a7af3b4433d57eec573a6b78ba52377c6fd8
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 17:34:38 -0500

    Update 07_create_users_table.sql

--- cripts/07_create_users_table.sql
+++ cripts/07_create_users_table.sql
@@ -1,1 +1,9 @@
-
+-- ================================
+-- TABLA: Usuario
+-- ================================
+CREATE TABLE Usuario (
+    id_usuario  INT AUTO_INCREMENT PRIMARY KEY,
+    nombre      VARCHAR(100) NOT NULL,
+    correo      VARCHAR(100) UNIQUE NOT NULL,
+    contrasena  VARCHAR(255) NOT NULL
+);
