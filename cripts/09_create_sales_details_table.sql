e79dea7e3fb3ad950e46b7f6d2cd4e3bd6fc37d3
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 17:35:48 -0500

    Update 09_create_sales_details_table.sql

--- cripts/09_create_sales_details_table.sql
+++ cripts/09_create_sales_details_table.sql
@@ -1,1 +1,12 @@
-
+-- ================================
+-- TABLA: Detalle_Venta
+-- ================================
+CREATE TABLE Detalle_Venta (
+    id_detalle     INT AUTO_INCREMENT PRIMARY KEY,
+    cantidad       INT NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL,
+    id_venta       INT,
+    id_producto    INT,
+    FOREIGN KEY (id_venta) REFERENCES Venta(id_venta),
+    FOREIGN KEY (id_producto) REFERENCES Producto(id_producto)
+);
