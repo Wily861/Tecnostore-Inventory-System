e628e6861eefbaec56dd3a5f1cea584971be7bac
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 17:35:11 -0500

    Update 08_create_sales_table.sql

--- cripts/08_create_sales_table.sql
+++ cripts/08_create_sales_table.sql
@@ -1,1 +1,12 @@
-
+-- ================================
+-- TABLA: Venta
+-- ================================
+CREATE TABLE Venta (
+    id_venta   INT AUTO_INCREMENT PRIMARY KEY,
+    estado     VARCHAR(50) NOT NULL,
+    fecha      DATE NOT NULL,
+    id_cliente INT,
+    id_usuario INT,
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+);
